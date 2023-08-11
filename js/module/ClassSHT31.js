73a2da4fd145fb2ca53638c4598e39f83b32bdb5
Aleksandr <arrarrat65@gmail.com>
2023-08-11 09:37:31 +0400

    fix: 1st draft of working version

--- js/module/ClassSHT31.js
+++ js/module/ClassSHT31.js
@@ -5,13 +5,14 @@
 class ClassSHT31 {
     /**
      * @constructor
-     * @param {Object} _Bus   - - объект класса I2C Bus
-     * @param {Hex} _address   - - адрес - 0х44 или 0х45
-     * @param {string} _repeatability - повторяемость
+     * @param {Object} _bus   - объект класса I2C Bus
+     * @param {Hex} _address  - адрес - 0х44 или 0х45
+     * @param {string} _rep   - повторяемость (repeatability)
      */
-    constructor() {
+    constructor(_Bus, _address, _rep) {
         this._name = 'ClassSHT31'; //переопределяем имя типа
-		this._sensor = require('ttt').connect({i2c: _Bus, address: _address, repeatabulity: _repeatability});
+		this._sensor = require('https://raw.githubusercontent.com/AlexGlgr/ModuleMeteoSHT31/fork-Alexander/js/module/meteo-sensor.min.js').connect({i2c: _Bus, address: _address, repeatabulity: _rep});
+        this._heaterOn = false;
     }
     /**
      * @method
@@ -31,6 +32,44 @@ class ClassSHT31 {
                 break;
         }
     }
+    /**
+     * @method
+     * Включает/выключает нагреватель
+     * @returns {boolean}  - нагреватель включен или выключен
+     */
+    SwitchHeater() {
+        if (this._heaterOn) {
+            this._sensor.heaterOff();
+            this._heaterOn = false;
+        }
+        else {
+            this._sensor.heaterOn();
+            this._heaterOn = true;
+        }
+
+        return this._heaterOn;
+    }
+    /**
+     * @method
+     * Сбрасывает датчик
+     * @returns {string}  - подтверждение, что датчик сброшен
+     */
+    Reset() {
+        this._sensor.reset();
+
+        return "Sensor reset";
+    }
+
+    /*
+    meteoSensor.read(function(err, data) {
+    if (err) {
+        print(err);
+    } else {
+        print("Temp is: "+data.tempC+" C," + "Temp is: "+data.tempF+" F,"+
+        "Temp is: "+data.tempK+" K,"+"Hum is: "+data.humidity+" %");
+    }
+    });
+    */
 }
 	
 
