9dee12c919cff002d799040424352cadf42dc8a2
Aleksandr <arrarrat65@gmail.com>
2023-08-22 18:10:21 +0400

    fix:

--- js/module/BaseClassSHT31.js
+++ js/module/BaseClassSHT31.js
@@ -6,7 +6,7 @@ function SHT3C(_i2c) {
   
   /// Write a 16 bit command to the SHT3C
   SHT3C.prototype.w = function(d) {
-    this.i2c.writeTo(0x44,[d>>8,d&255]);
+    this.i2c.writeTo(0x70,[d>>8,d&255]);
   }
   
   /// Read with callback({humidity,temp})
@@ -15,7 +15,7 @@ function SHT3C(_i2c) {
     this.w(0x5C24); // read RH first, normal power, clock stretch
     var sht = this;
     setTimeout(function() {
-      var d = new DataView(sht.i2c.readFrom(0x44,6).buffer);
+      var d = new DataView(sht.i2c.readFrom(0x70,6).buffer);
       sht.w(0xB098); // sleep
       callback({ humidity : d.getUint16(0)/655.36,
                  temp : d.getUint16(3)*175/65536 - 45 });
