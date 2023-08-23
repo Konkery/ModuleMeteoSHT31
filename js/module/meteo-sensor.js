bf853dc0ba58cf6ecc4b96f2c1bc1e85c3ea6f9b
Aleksandr <arrarrat65@gmail.com>
2023-08-23 12:04:04 +0400

    fix:

--- js/module/meteo-sensor.js
+++ js/module/meteo-sensor.js
@@ -1,9 +1,9 @@
 function TroykaMeteoSensor(opts) {
-  console.log(opts);
   opts = opts || {};
   var _i2c = opts.i2c || PrimaryI2C;
   var _address = opts.address || 0x44;
   var _repeatability = opts.repeatability || 'HIGH';
+  console.log(opts);
 
   this.setRepeatability = function(value) {
     _repeatability = value;
