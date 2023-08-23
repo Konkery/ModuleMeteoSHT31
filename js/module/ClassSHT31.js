9d956fe44b19a1902ef937270f3cca2977a695f2
Aleksandr <arrarrat65@gmail.com>
2023-08-23 12:15:49 +0400

    fix:

--- js/module/ClassSHT31.js
+++ js/module/ClassSHT31.js
@@ -11,7 +11,7 @@ class ClassSHT31 extends ClassMiddleSensor {
     constructor(_opts) {
         ClassMiddleSensor.apply(this, [_opts]);
         this._name = 'ClassSHT31'; //переопределяем имя типа
-		this._sensor = require('https://raw.githubusercontent.com/AlexGlgr/ModuleMeteoSHT31/fork-Alexander/js/module/meteo-sensor.min.js').connect({i2c: (new ClassI2CBus).AddBus({sda: _opts._Pins[0], scl: _opts._Pins[1], bitrate: 100000}).IDbus, address: 0x44, repeatability: 'LOW'});
+		this._sensor = require('https://raw.githubusercontent.com/AlexGlgr/ModuleMeteoSHT31/fork-Alexander/js/module/meteo-sensor.min.js').connect({i2c: (new ClassI2CBus).AddBus({sda: _opts._Pins[0], scl: _opts._Pins[1], bitrate: 100000}).IDbus, address: _opts._Address, repeatability: _opts._Repeatability});
         this._minPeriod = 1000;
         this._usedChannels = [];
         this._interval;
